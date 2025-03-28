e42e7bcdb1400c4d38d17c2840bffa003121848d
AbdelrahmanAbouzaid <a.abouzaid404@gmail.com>
2025-03-28 03:10:27 +0200

    Implement Security Module and Identity Package

---
+++ Company.Web.PL/Controllers/AccountController.cs
@@ -0,0 +1,92 @@
+﻿using Company.Web.DAL.Models;
+using Company.Web.PL.Dtos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.Web.PL.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> signInManager;
+
+        public AccountController(UserManager<AppUser> _userManager, SignInManager<AppUser> signInManager)
+        {
+            userManager = _userManager;
+            this.signInManager = signInManager;
+        }
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.FindByNameAsync(model.UserName);
+                if (user is null)
+                {
+                    user = await userManager.FindByEmailAsync(model.Email);
+                    if (user is null)
+                    {
+                        user = new AppUser
+                        {
+                            UserName = model.UserName,
+                            FirstName = model.FirstName,
+                            LastName = model.LastName,
+                            Email = model.Email,
+                            IsAgree = model.IsAgree,
+                        };
+                        var result = await userManager.CreateAsync(user, model.Password);
+                        if (result.Succeeded)
+                            return RedirectToAction("SignIn");
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError("", error.Description);
+                        }
+                    }
+                }
+                ModelState.AddModelError("", "Invalid SignUp !");
+
+            }
+
+            return View(model);
+
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.FindByEmailAsync(model.Email);
+                if (user is not null)
+                {
+                    var flag = await userManager.CheckPasswordAsync(user, model.Password);
+                    if (flag)
+                    {
+                        var result = await signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, false);
+                        if (result.Succeeded)
+                            return RedirectToAction("Index", "Home");
+                    }
+                }
+                ModelState.AddModelError("", "Invalid SignIn !");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async new Task<IActionResult> SignOut()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction(nameof(SignIn));
+        }
+    }
+}
---
+++ Company.Web.PL/Dtos/SignInDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Web.PL.Dtos
+{
+    public class SignInDto
+    {
+        [Required(ErrorMessage = "Email is required !")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required !")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
