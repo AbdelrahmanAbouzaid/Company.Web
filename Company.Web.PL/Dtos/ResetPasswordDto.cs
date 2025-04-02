d7ebd37fa4c5a50cc714ac1612446a214a26866a
AbdelrahmanAbouzaid <a.abouzaid404@gmail.com>
2025-04-02 22:06:21 +0200

    Update Securoty and Add Users And Roles Controllers

---
+++ Company.Web.PL/Dtos/ResetPasswordDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Web.PL.Dtos
+{
+    public class ResetPasswordDto
+    {
+        [Required(ErrorMessage = "Password Is Required!")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password Is Required!")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(NewPassword), ErrorMessage = "Confirm Password dose not match Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
